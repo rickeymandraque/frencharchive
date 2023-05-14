d4ad5902b6471127454919ee7524955e3e294ef7
AirbnbEcoPlus <adf7bn@gmail.com>
2023-05-14 18:32:42 +0200

    FrenchStreamProvider Testing

--- FrenchStreamProvider/src/main/kotlin/com/lagradost/FrenchStreamSettingsFragment.kt
+++ FrenchStreamProvider/src/main/kotlin/com/lagradost/FrenchStreamSettingsFragment.kt
@@ -55,23 +55,10 @@ class FrenchStreamSettingsFragment(
         return plugin.resources!!.getString(id)
     }
 
-    @SuppressLint("CommitPrefEdits")
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val sharedPref = activity?.getSharedPreferences(
-                getString("frenchstream"), Context.MODE_PRIVATE)
-        super.onViewCreated(view, savedInstanceState)
-        val urlField = view.findViewById<EditText>(R.id.urlField)
-        val saveBtn = view.findViewById<Button>(R.id.saveBtn)
-        // Change Url
-        if (sharedPref != null) {
-            urlField.setText(sharedPref.getString("url", ""))
-            sharedPref.getString("url", "")?.let { frenchStreamProvider.changeUrl(it) }
-        }
 
-        saveBtn.setOnClickListener(View.OnClickListener {
-            frenchStreamProvider.changeUrl(urlField.text.toString())
-            sharedPref?.edit()?.putString("url", urlField.text.toString())
-        })
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        
+        
     }
 }
 
