fd044545ab26a82c98b294b82eabbd10ebcdf0d0
AirbnbEcoPlus <adf7bn@gmail.com>
2024-02-06 20:23:41 +0100

    Update frenchstream url

--- FrenchStreamProvider/src/main/kotlin/com/lagradost/FrenchStreamProviderPlugin.kt
+++ FrenchStreamProvider/src/main/kotlin/com/lagradost/FrenchStreamProviderPlugin.kt
@@ -11,5 +11,6 @@ class FrenchStreamProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(FrenchStreamProvider())
+        registerExtractorAPI(DoodStreamExtractor())
     }
 }
